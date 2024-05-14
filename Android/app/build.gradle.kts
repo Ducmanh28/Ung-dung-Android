46ad342e72b35a8c3f963ce950793e9d93a59d89
Ducmanh28 <95476360+Ducmanh28@users.noreply.github.com>
2024-05-14 23:21:48 +0700

    cập nhật viewflipper

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -32,12 +32,17 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.4.0") // Thay thế thư viện design cũ
+    implementation("com.google.android.gms:play-services-maps:18.0.2")
+    implementation("androidx.recyclerview:recyclerview:1.2.1") // Phiên bản ổn định thay vì alpha
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
---
+++ Android/app/src/main/java/com/example/ungdungbanhang/activity/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.ungdungbanhang.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ungdungbanhang.R;
+import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    androidx.appcompat.widget.Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewmanhinhchinh;
+    NavigationView navigationView;
+    ListView listViewmanhinhchinh;
+    DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Anhxa();
+        ActionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionViewFlipper() {
+        ArrayList<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://www.thongtincongnghe.com/sites/default/files/images/2012/1/13/img-1326420488-2.jpg");
+        mangquangcao.add("https://www.thongtincongnghe.com/sites/default/files/images/2012/1/13/img-1326420488-3.jpg");
+        mangquangcao.add("https://www.thongtincongnghe.com/sites/default/files/images/2012/1/13/img-1326420488-4.jpg");
+        for(int i = 0;i<mangquangcao.size();i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Picasso.get().load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        Animation animation_slide_in_right = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        Animation animation_slide_out_right = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_out_right);
+        viewFlipper.setInAnimation(animation_slide_in_right);
+        viewFlipper.setOutAnimation(animation_slide_out_right);
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void Anhxa() {
+        toolbar = (Toolbar) findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewflipper);
+        recyclerViewmanhinhchinh = (RecyclerView) findViewById(R.id.recyclerview);
+        navigationView = (NavigationView) findViewById(R.id.navigationview);
+        listViewmanhinhchinh = (ListView) findViewById(R.id.listviewmanhinhchinh);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+    }
+}
